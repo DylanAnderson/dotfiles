ba0e4577246b6aca416a8630c0493f102587fa39
Dylan Anderson <dylanz0o4.da@gmail.com>
2018-07-06 15:52:34 -0600

    Removed prezto from install script.

--- install.sh
+++ install.sh
@@ -10,13 +10,6 @@ stow tmux
 
 # zsh
 echo "Installing zsh files..."
-# Get the prezto files, copy various dotfiles
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
 # Change shell to zsh
 chsh -s $(which zsh)
 rm -f ~/.zshrc
